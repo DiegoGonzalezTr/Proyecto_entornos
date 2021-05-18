10c32ccd6d00122c00164cc9f684a4699c40b799
alumno <alumno@aula20711.cocim.aulas.es>
2021-05-18 12:02:45 +0200

    0.4.1 He añadido cambios

--- Proyecto_Final_Entornos/src/calculadoraIP/CalculadoraIP.java
+++ Proyecto_Final_Entornos/src/calculadoraIP/CalculadoraIP.java
@@ -1,16 +1,18 @@
 package calculadoraIP;
 
+import java.util.Scanner;
+
 public class CalculadoraIP {
 	//Ips y máscaras.
-	int ip_rista1;
-	int ip_rista2;
-	int ip_rista3;
-	int ip_rista4;
+	static int ip_rista1;
+	static int ip_rista2;
+	static int ip_rista3;
+	static int ip_rista4;
 	
-	int mascara_rista1;
-	int mascara_rista2;
-	int mascara_rista3;
-	int mascara_rista4;
+	static int mascara_rista1;
+	static int mascara_rista2;
+	static int mascara_rista3;
+	static int mascara_rista4;
 	
 	//Direcciones Ips, y máscaras concatenadas.
 	String ip_concatenada;
@@ -20,19 +22,21 @@ public class CalculadoraIP {
 	String ip_broadcast;
 	String mascara_concatenada;
 	
+	//Scanner para introducir la IP.
+	Scanner sc = new Scanner(System.in);
 
 	public CalculadoraIP(int ip_rista1, int ip_rista2, int ip_rista3, int ip_rista4, int mascara_rista1,
 			int mascara_rista2, int mascara_rista3, int mascara_rista4, String ip_concatenada, String ip_red,
 			String ip_primera_valida, String ip_ultima_valida, String ip_broadcast, String mascara_concatenada) {
 		super();
-		this.ip_rista1 = ip_rista1;
-		this.ip_rista2 = ip_rista2;
-		this.ip_rista3 = ip_rista3;
-		this.ip_rista4 = ip_rista4;
-		this.mascara_rista1 = mascara_rista1;
-		this.mascara_rista2 = mascara_rista2;
-		this.mascara_rista3 = mascara_rista3;
-		this.mascara_rista4 = mascara_rista4;
+		CalculadoraIP.ip_rista1 = ip_rista1;
+		CalculadoraIP.ip_rista2 = ip_rista2;
+		CalculadoraIP.ip_rista3 = ip_rista3;
+		CalculadoraIP.ip_rista4 = ip_rista4;
+		CalculadoraIP.mascara_rista1 = mascara_rista1;
+		CalculadoraIP.mascara_rista2 = mascara_rista2;
+		CalculadoraIP.mascara_rista3 = mascara_rista3;
+		CalculadoraIP.mascara_rista4 = mascara_rista4;
 		this.ip_concatenada = ip_concatenada;
 		this.ip_red = ip_red;
 		this.ip_primera_valida = ip_primera_valida;
@@ -47,7 +51,7 @@ public class CalculadoraIP {
 	}
 	
 	public void setIp_rista1(int ip_rista1) {
-		this.ip_rista1 = ip_rista1;
+		CalculadoraIP.ip_rista1 = ip_rista1;
 	}
 	
 	public int getIp_rista2() {
@@ -55,7 +59,7 @@ public class CalculadoraIP {
 	}
 	
 	public void setIp_rista2(int ip_rista2) {
-		this.ip_rista2 = ip_rista2;
+		CalculadoraIP.ip_rista2 = ip_rista2;
 	}
 
 	public int getIp_rista3() {
@@ -63,7 +67,7 @@ public class CalculadoraIP {
 	}
 
 	public void setIp_rista3(int ip_rista3) {
-		this.ip_rista3 = ip_rista3;
+		CalculadoraIP.ip_rista3 = ip_rista3;
 	}
 
 	public int getIp_rista4() {
@@ -71,7 +75,7 @@ public class CalculadoraIP {
 	}
 
 	public void setIp_rista4(int ip_rista4) {
-		this.ip_rista4 = ip_rista4;
+		CalculadoraIP.ip_rista4 = ip_rista4;
 	}
 
 	public int getMascara_rista1() {
@@ -79,7 +83,7 @@ public class CalculadoraIP {
 	}
 
 	public void setMascara_rista1(int mascara_rista1) {
-		this.mascara_rista1 = mascara_rista1;
+		CalculadoraIP.mascara_rista1 = mascara_rista1;
 	}
 
 	public int getMascara_rista2() {
@@ -87,7 +91,7 @@ public class CalculadoraIP {
 	}
 
 	public void setMascara_rista2(int mascara_rista2) {
-		this.mascara_rista2 = mascara_rista2;
+		CalculadoraIP.mascara_rista2 = mascara_rista2;
 	}
 
 	public int getMascara_rista3() {
@@ -95,7 +99,7 @@ public class CalculadoraIP {
 	}
 
 	public void setMascara_rista3(int mascara_rista3) {
-		this.mascara_rista3 = mascara_rista3;
+		CalculadoraIP.mascara_rista3 = mascara_rista3;
 	}
 
 	public int getMascara_rista4() {
@@ -103,7 +107,7 @@ public class CalculadoraIP {
 	}
 
 	public void setMascara_rista4(int mascara_rista4) {
-		this.mascara_rista4 = mascara_rista4;
+		CalculadoraIP.mascara_rista4 = mascara_rista4;
 	}
 
 	public String getIp_concatenada() {
@@ -162,6 +166,11 @@ public class CalculadoraIP {
 				+ ", ip_concatenada=" + ip_concatenada + ", ip_red=" + ip_red + ", ip_primera_valida="
 				+ ip_primera_valida + ", ip_ultima_valida=" + ip_ultima_valida + ", ip_broadcast=" + ip_broadcast
 				+ ", mascara_concatenada=" + mascara_concatenada + "]";
+	}
+	
+	public static void Insertar_IP()
+	{	System.out.println("Inserte 1º Valor de la IP:");
+		ip_rista1=0;
 	}
 	
 	public static void main(String[] args) {
---
+++ Proyecto_Final_Entornos/src/calculadoraIP/Principal.java
@@ -0,0 +1,16 @@
+package calculadoraIP;
+
+public class Principal {
+
+	/* Aquí se puede añadir la ejecución del programa
+	 * Java, para que se pueda utilizar.
+	 * Lo podemos dejar para el final, mientras tanto
+	 * usamos el main que hay en la clase calculadora.
+	 * Si quereis podeis crear nuevas clases para
+	 * distintas funciones.
+	 */
+	
+	public static void main(String[] args) {
+	}
+
+}
