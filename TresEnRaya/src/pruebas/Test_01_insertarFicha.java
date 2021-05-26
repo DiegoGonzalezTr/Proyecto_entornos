d21c4fdd4f8f24640d20e120157663829629df94
alumno <alumno@aula20711.cocim.aulas.es>
2021-05-26 09:11:11 +0200

    Package de pruebas

---
+++ TresEnRaya/src/pruebas/Test_01_insertarFicha.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test_01_insertarFicha {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
